91bcd52f0337ca10915eeab1ba109eb3dd9227cd
dyooo123 <aldyoputra21@gmail.com>
2023-08-23 14:37:52 +0700

    edit : edit sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -27,10 +27,10 @@
                 <i class="fas fa-tachometer-alt mr-3"></i>
                 Creative Crews
             </a>
-            <a href="{{ url('admin/partnership')}}"" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-sticky-note mr-3"></i>
+            <a href="{{ url('admin/partnership')}}"class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+                <i class="fas fa-tachometer-alt mr-3"></i>
                 Partnership
-            </a>
+            </a> 
             <a href="{{ url('admin/blog')}}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
                 <i class="fas fa-tachometer-alt mr-3"></i>
                 Blog
