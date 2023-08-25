c748ec615e541ebb86b1b855f469344169b0d4f6
dyooo123 <aldyoputra21@gmail.com>
2023-08-25 15:15:19 +0700

    edit : debug error cancel button

--- resources/views/admin/event/index.blade.php
+++ resources/views/admin/event/index.blade.php
@@ -685,7 +685,7 @@
                                 <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                                     <button type="submit"
                                         class="w-full sm:w-auto justify-center text-white inline-flex bg-primary hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Simpan</button>
-                                    <button data-modal-toggle="createProductModal" type="button"
+                                        <button button type="button" data-drawer-dismiss="drawer-update-product"
                                         class="w-full justify-center sm:w-auto text-secondary inline-flex items-center bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                         <svg class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
                                             xmlns="http://www.w3.org/2000/svg">
