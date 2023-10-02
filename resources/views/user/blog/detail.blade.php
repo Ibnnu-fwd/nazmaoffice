a4b3f5f27609d7ad04a033acfe094f48cd87b8a9
Aldyo Putra <106857082+dyooo123@users.noreply.github.com>
2023-10-02 10:51:03 +0700

    edit : image height

--- resources/views/user/blog/detail.blade.php
+++ resources/views/user/blog/detail.blade.php
@@ -53,7 +53,7 @@
                 <div class="grid grid-cols-2 gap-x-6 gap-y-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
                     @foreach ($blogs as $data)
                         <a href="{{ route('user.blog.detail', $data->slug) }}">
-                            <img class="w-full bg-gray-200 h-50 rounded-lg object-cover object-center blur-mode"
+                            <img class="w-full bg-gray-200 h-50 rounded-lg object-cover object-center blur-mode related-image"
                                 src="{{ asset('storage/blogs/thumbnail/' . $data->thumbnail) }}" alt="">
 
                             <p class="mt-5 text-md font-normal leading-6 text-gray-400">
@@ -71,8 +71,8 @@
                 @if ($blogs->count() > 3)
                     <div onclick="window.location.href='{{ route('user.blog.detail', $blogs->last()->slug) }}'"
                         class="grid grid-cols-1 mt-10 md:grid-cols-2 lg:grid-cols-3 gap-6 rounded-xl border border-gray-200 p-4 md:border-0 md:p-0">
-                        <img src="{{ asset('assets/images/hero2.jpg') }}"
-                            class="hidden h-52 mx-auto lg:inline-block object-center object-cover blur-mode rounded-lg"
+                        <img src="{{ asset('assets/images/hero2.jpg') }}" 
+                            class="hidden h-52 mx-auto lg:inline-block object-center object-cover blur-mode rounded-lg related-image"
                             alt="">
                         <div class="col-span-2">
                             <span class="text-gray-400 text-sm">
@@ -107,7 +107,7 @@
                 // remove style in image except main image
                 $('img').not('#main-image').removeAttr('style');
                 // style img in content
-                $('img').not('#main-image').addClass(
+                $('img').not('#main-image, .related-image').addClass(
                     'blur-mode my-4 w-[40rem] h-full mx-auto object-cover object-center border border-gray-200 rounded-xl'
                 );
             });
